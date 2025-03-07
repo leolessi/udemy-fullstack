e6f754cd353c12416618e9ec4bb3495ed9336e34
Leonardo Lessi <leonardolessi@gmail.com>
2025-03-07 00:06:29 -0300

    99bottles terceira forma

--- Sections 03 - (Javascript)/12 - While Loops - 99 Bottles of Beer/99_bottles_beer.js
+++ Sections 03 - (Javascript)/12 - While Loops - 99 Bottles of Beer/99_bottles_beer.js
@@ -22,24 +22,36 @@
 
 /////////////////////////////////////////////////////////////////////////////////
 
-var numberOfBottles = 100
-while (numberOfBottles >= 0) {
-    var bottleWord = "bottles";
-    if (numberOfBottles === 1) {
-        bottleWord = "bottle";
-        console.log("\n" + numberOfBottles + " " + bottleWord + " of beer on the wall, " + numberOfBottles + " " + bottleWord + " of beer.");
-        console.log("Take one down, pass it around, no more bottles of beer on the wall.")
-    }
-    else if (numberOfBottles === 0) {
-        console.log("\nNo more bottles of beer on the wall, no more bottles of beer.");
-        console.log("Go to the store and buy some more, " + (numberOfBottles + 99) + " bottles of beer on the wall.");
-    }
-    else {
-        console.log("\n" + numberOfBottles + " " + bottleWord + " of beer on the wall, " + numberOfBottles + " bottles of beer.");
-        console.log("Take one down and pass it around, " + numberOfBottles + " " + bottleWord + " of beer on the wall.");
-    }
-    numberOfBottles--;
-}
+// var numberOfBottles = 100
+// while (numberOfBottles >= 0) {
+//     var bottleWord = "bottles";
+//     if (numberOfBottles === 1) {
+//         bottleWord = "bottle";
+//         console.log("\n" + numberOfBottles + " " + bottleWord + " of beer on the wall, " + numberOfBottles + " " + bottleWord + " of beer.");
+//         console.log("Take one down, pass it around, no more bottles of beer on the wall.")
+//     }
+//     else if (numberOfBottles === 0) {
+//         console.log("\nNo more bottles of beer on the wall, no more bottles of beer.");
+//         console.log("Go to the store and buy some more, " + (numberOfBottles + 99) + " bottles of beer on the wall.");
+//     }
+//     else {
+//         console.log("\n" + numberOfBottles + " " + bottleWord + " of beer on the wall, " + numberOfBottles + " bottles of beer.");
+//         console.log("Take one down and pass it around, " + numberOfBottles + " " + bottleWord + " of beer on the wall.");
+//     }
+//     numberOfBottles--;
+// }
 
 /////////////////////////////////////////////////////////////////////////////////
 
+for (var i = 99; i >= 1; i--) {
+    if (i > 1) {
+        console.log(`\n${i} bottles of beer on the wall, ${i} bottle of beer.`);
+        console.log(`Take one down and pass it around, ${i - 1} bottles of beer on the wall.`);
+    }
+    else {
+        console.log(`\n${i} bottle of beer on the wall, ${i} bottle of beer.`);
+        console.log('Take one down and pass it around, no more bottles of beer on the wall.');
+    }
+}
+console.log("\nNo more bottles of beer on the wall, no more bottles of beer. ");
+console.log("Go to the store and buy some more, 99 bottles of beer on the wall.");
