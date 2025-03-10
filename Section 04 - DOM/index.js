76beba3c8b4f7471ed9b6df79a179d007e53aedd
Leonardo Lessi <leonardolessi@gmail.com>
2025-03-10 13:23:03 -0300

    mudança de sections para section e adicionando pasta DOM

---
+++ Section 04 - DOM/index.js
@@ -0,0 +1,6 @@
+// var heading = document.firstElementChild.lastElementChild.firstElementChild;
+// heading.innerHTML = "I am a heading";
+// heading.style.color = "red";
+
+// var thirList = document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "I am the last list item";
+
